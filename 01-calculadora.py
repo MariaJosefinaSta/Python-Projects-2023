42237f4ac41df51d69d378a898fa44e62f458eef
Maria Josefina Stabile <mariajosefinastabile@gmail.com>
2023-03-13 19:02:58 -0300

    Defining message, error in print marked as comment

--- 01-calculadora.py
+++ 01-calculadora.py
@@ -12,3 +12,14 @@ suma = n1 + n2
 resta = n1 - n2
 multi = n1 * n2
 division = n1 / n2
+
+# print(f {division} {es el resultado de la dividir}) MAL
+
+mensaje = f"""
+Para los números {n1} y {n2}.
+el resultado de la suma es {suma}.
+el resultado de la resta es {resta}.
+el resultado de la multi es {multi}.
+el resultado de la división es {division}.
+"""
+print(mensaje)
