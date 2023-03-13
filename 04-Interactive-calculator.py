c7b176563eb7f3479bf1ee97eaeed42e443148b3
Maria Josefina Stabile <mariajosefinastabile@gmail.com>
2023-03-13 19:46:40 -0300

    First slogan: check if a number is entered

--- 04-Interactive-calculator.py
+++ 04-Interactive-calculator.py
@@ -2,3 +2,15 @@
 # If they have already entered a number they will ask you to enter an operation.
 # Then ask to enter a second value and the operator.
 # And the result will be assigned as the first value you enter into the calculator for a next operation.
+
+print("Welcome to the calculator")
+print("To exit type exit")
+print("You can add, subtract, divide or multiply")
+
+resultado = " "
+
+while True:
+    if not resultado:
+        resultado = input("ingrese número")
+        if resultado.lower() == "salir":
+            break
