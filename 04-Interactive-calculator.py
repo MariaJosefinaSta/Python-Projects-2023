428b1ade057737d76222bffdea780eb218f85d1b
Maria Josefina Stabile <mariajosefinastabile@gmail.com>
2023-03-13 19:40:42 -0300

    It's short, a single commit

---
+++ 04-Interactive-calculator.py
@@ -0,0 +1,4 @@
+# The calculator should check if a number has already been entered.
+# If they have already entered a number they will ask you to enter an operation.
+# Then ask to enter a second value and the operator.
+# And the result will be assigned as the first value you enter into the calculator for a next operation.
