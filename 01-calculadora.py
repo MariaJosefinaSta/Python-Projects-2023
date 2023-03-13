9fd0f5dc2fe0202e2a2f494abbec4c9eb402d0b9
Maria Josefina Stabile <mariajosefinastabile@gmail.com>
2023-03-13 19:00:54 -0300

    Defining arithmetic accounts

--- 01-calculadora.py
+++ 01-calculadora.py
@@ -4,3 +4,11 @@ n1 = input("ingresa primero número")
 n2 = input("ingresa segundo número")
 
 print(n1, n2)
+
+n1 = int(n1)
+n2 = int(n2)
+
+suma = n1 + n2
+resta = n1 - n2
+multi = n1 * n2
+division = n1 / n2
