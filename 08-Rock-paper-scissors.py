6d2c54e775aa5067c2adbd70912a47db5a5f9938
Maria Josefina Stabile <mariajosefinastabile@gmail.com>
2023-04-04 06:53:34 -0300

    It doesn't work because it didn't have Print Play

--- 08-Rock-paper-scissors.py
+++ 08-Rock-paper-scissors.py
@@ -2,6 +2,7 @@ import random
 
 
 def play():
+
     user = input(
         "Whats your choise? 'r' for rock, 'p' for paper, 's' for scissors")
     # la computadora elige una opción random
@@ -21,3 +22,6 @@ def is_win(player, opponent):
     if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') \
             or (player == 'p' and opponent == 'r'):
         return True
+
+
+print(play())
